c59a283271279ad5331f4dac037758720f68c134
tmnj <triet.nguyen@usace.army.mil>
2025-07-15 14:42:55 -0400

    build plugin image from base python

--- compose.sh
+++ compose.sh
@@ -5,9 +5,9 @@ set -Eeo pipefail
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-COMPOSECMD_INFRAS="docker compose -f docker-compose/docker-compose.infras.yaml"
-COMPOSECMD_PLUGIN="docker compose -f docker-compose/docker-compose.plugin.yaml"
-COMPOSECMD_PLUGIN_IT="docker compose -f docker-compose/docker-compose.plugin-it.yaml"
+COMPOSE_INFRAS_PATH="docker-compose/docker-compose.infras.yaml"
+COMPOSE_PLUGIN_PATH="docker-compose/docker-compose.plugin.yaml"
+COMPOSE_PLUGIN_IT_PATH="docker-compose/docker-compose.plugin-it.yaml"
 
 
 for arg in "$@"
@@ -16,63 +16,63 @@ do
         "up")
             shift
             detach_option=""; if [ "$1" = "-d" ]; then detach_option=$1; shift; fi
-            cmd="$COMPOSECMD_INFRAS -f docker-compose/docker-compose.plugin.yaml up --remove-orphans $detach_option --build $@"
+            cmd="docker compose -f $COMPOSE_INFRAS_PATH -f $COMPOSE_PLUGIN_IT_PATH up --remove-orphans $detach_option --build $@"
             echo -e "$cmd"
             $cmd
             ;;
         "infras-up")
             shift
             detach_option=""; if [ "$1" = "-d" ]; then detach_option=$1; shift; fi
-            cmd="$COMPOSECMD_INFRAS -p cc-infras up --remove-orphans $detach_option --build $@"
+            cmd="docker compose -f $COMPOSE_INFRAS_PATH -p cc-infras up --remove-orphans $detach_option --build $@"
             echo -e "$cmd"
             $cmd
             ;;
         "plugin-up")
             shift
             detach_option=""; if [ "$1" = "-d" ]; then detach_option=$1; shift; fi
-            cmd="$COMPOSECMD_PLUGIN -p cc-plugin up --remove-orphans $detach_option --build $@"
+            cmd="docker compose -f $COMPOSE_PLUGIN_PATH -p cc-plugin up --remove-orphans $detach_option --build $@"
             echo -e "$cmd"
             $cmd
             ;;
         "plugin-it-up")
             shift
             detach_option=""; if [ "$1" = "-d" ]; then detach_option=$1; shift; fi
-            cmd="$COMPOSECMD_PLUGIN_IT -p cc-plugin up --remove-orphans $detach_option --build $@"
+            cmd="docker compose -f $COMPOSE_PLUGIN_IT_PATH -p cc-plugin up --remove-orphans $detach_option --build $@"
             echo -e "$cmd"
             $cmd
             ;;
         "down")
             shift
-            cmd="$COMPOSECMD_INFRAS -f docker-compose/docker-compose.plugin.yaml -f docker-compose/docker-compose.plugin-it.yaml down --remove-orphans $@"
+            cmd="docker compose -f $COMPOSE_INFRAS_PATH -f $COMPOSE_PLUGIN_PATH -f $COMPOSE_PLUGIN_IT_PATH down --remove-orphans $@"
             echo -e "$cmd"
             $cmd
             ;;
         "infras-down")
             shift
-            cmd="$COMPOSECMD_INFRAS down --remove-orphans $@"
+            cmd="docker compose -f $COMPOSE_INFRAS_PATH down --remove-orphans $@"
             echo -e "$cmd"
             $cmd
             ;;
         "plugin-down")
             shift
-            cmd="$COMPOSECMD_PLUGIN down --remove-orphans $@"
+            cmd="docker compose -f $COMPOSE_PLUGIN_PATH down --remove-orphans $@"
             echo -e "$cmd"
             $cmd
             ;;
         "watch")
             shift
-            cmd="$COMPOSECMD_INFRAS -f docker-compose/docker-compose.plugin.yaml up --remove-orphans --build --watch $@"
+            cmd="docker compose -f $COMPOSE_INFRAS_PATH -f $COMPOSE_PLUGIN_PATH up --remove-orphans --build --watch $@"
             echo -e "$cmd"
             $cmd
             ;;
         "logs")
             shift
-            cmd="$COMPOSECMD_INFRAS -f docker-compose/docker-compose.plugin.yaml logs -f $@"
+            cmd="docker compose -f $COMPOSE_INFRAS_PATH -f $COMPOSE_PLUGIN_PATH logs -f $@"
             echo -e "$cmd"
             $cmd
             ;;
         "volume-prune")
-            cmd="$COMPOSECMD_INFRAS down --remove-orphans -v"
+            cmd="docker compose -f $COMPOSE_INFRAS_PATH down --remove-orphans -v"
             echo -e "$cmd"
             $cmd
             ;;
